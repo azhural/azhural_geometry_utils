b020cc55274de1c4bdef6e438937894fb0d2720c
Patrick Coenen <patrick@coenenmail.de>
2019-10-30 12:33:33 +0100

    fixed type

--- include/azhural_geometry_utils/transformations.h
+++ include/azhural_geometry_utils/transformations.h
@@ -10,7 +10,7 @@ geometry_msgs::PoseStamped calculatePoseRelativeToPoseStamped(const geometry_msg
 
 geometry_msgs::Pose calculatePoseRelativeToTransform(const geometry_msgs::Transform &origin_to_intermediate, const geometry_msgs::Pose &intermediate_to_target);
 
-geometry_msgs::Transform calculateTransformRelativeToTransform(const geometry_msgs::TransformStamped &origin_to_intermediate, const geometry_msgs::Transform &intermediate_to_target);
+geometry_msgs::Transform calculateTransformRelativeToTransform(const geometry_msgs::Transform &origin_to_intermediate, const geometry_msgs::Transform &intermediate_to_target);
 
 void transformRelative(const tf2::Vector3 &origin_to_intermediate_t, const tf2::Quaternion &origin_to_intermediate_r, const tf2::Vector3 &intermediate_to_target_t, const tf2::Quaternion &intermediate_to_target_r,
                        tf2::Vector3 &translation_out, tf2::Quaternion &rotation_out);
